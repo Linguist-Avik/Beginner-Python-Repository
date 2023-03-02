d571159520298a5e6458f71bcf571884df276c48
Linguist-Avik <124483988+Linguist-Avik@users.noreply.github.com>
2023-03-03 00:03:50 +0530

    Update Automatic Pizza Order.py

--- Automatic Pizza Order.py
+++ Automatic Pizza Order.py
@@ -1,5 +1,7 @@
 # Automatic pizza order program
-#Small Pizza: $15; Medium Pizza: $20; Large Pizza: $25; Pepperoni for Small Pizza: +$2; Pepperoni for Medium or Large Pizza: +$3; Extra cheese for any size pizza: + $1
+#Small Pizza: $15; Medium Pizza: $20; Large Pizza: $25; 
+#Pepperoni for Small Pizza: +$2; Pepperoni for Medium or Large Pizza: +$3; 
+#Extra cheese for any size pizza: + $1
 
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
