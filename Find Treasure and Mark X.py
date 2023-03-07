39fea36e4c6948a276b4b076b77fc5c63abed351
Linguist-Avik <124483988+Linguist-Avik@users.noreply.github.com>
2023-03-07 21:19:34 +0530

    Update Find Treasure and Mark X.py

--- Find Treasure and Mark X.py
+++ Find Treasure and Mark X.py
@@ -6,8 +6,8 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-horizontal = int(position[0])
-vertical = int(position[1])
+horizontal = int(position[1])
+vertical = int(position[0])
 selected_row = map[horizontal - 1]
 selected_row = map[vertical - 1]
 map[horizontal - 1][vertical -1] = "X"
