6bc8ffba4d2b3e1ec89fbf3855b999dc01a0d7a4
Linguist-Avik <124483988+Linguist-Avik@users.noreply.github.com>
2023-03-10 00:47:23 +0530

    Update Rock Paper Scissors II.py

--- Rock Paper Scissors II.py
+++ Rock Paper Scissors II.py
@@ -1,4 +1,3 @@
-#Rock, Paper and Scissors game
 import random
 rock = '''  ROCK
     _______
@@ -26,33 +25,34 @@ scissors = '''  SCISSORS
       (____)
 ---.__(___)
 '''
-#game graphic as list
+#game graphics as list
 game_images = [rock, paper, scissors]
 
 #user input selection
 select = int(input('Enter numbers 0 for rock or 1 for paper or 2 for scissors: '))
-print('\nYou have selected:\n')
-print(game_images[select])
-
-#computer input selection
-random_selection = random.randint(0,2)
-print("\nComputer selects: \n")
-print(game_images[random_selection])
-
-#game logic
-if select > 2 and select < 0:
+if select > 2 or select < 0:
   print("Enter valid input")
-elif select == random_selection:
-  print("\n~ It is a tie ~")
-elif select == 0 and random_selection == 1:
-  print("\n~ Computer wins ~")
-elif select == 0 and random_selection == 2:
-  print("\n~ You win ~")
-elif select == 1 and random_selection == 1:
-  print("\n~You win~")
-elif select == 1 and random_selection == 2:
-  print("\n~ Computer wins ~")
-elif select == 2 and random_selection == 0:
-  print("\n~ Computer wins ~")
-elif select == 2 and random_selection == 1:
-  print("\n~ You win ~")
+else:
+  print('\nYou have selected:\n')
+  print(game_images[select])
+  
+  #computer input selecton
+  random_selection = random.randint(0,2)
+  print("\nComputer selects: \n")
+  print(game_images[random_selection])
+  
+  #game logic
+  if select == random_selection:
+    print("\n~ It is a tie ~")
+  elif select == 0 and random_selection == 1:
+    print("\n~ Computer wins ~")
+  elif select == 0 and random_selection == 2:
+    print("\n~ You win ~")
+  elif select == 1 and random_selection == 1:
+    print("\n~You win~")
+  elif select == 1 and random_selection == 2:
+    print("\n~ Computer wins ~")
+  elif select == 2 and random_selection == 0:
+    print("\n~ Computer wins ~")
+  elif select == 2 and random_selection == 1:
+    print("\n~ You win ~")
