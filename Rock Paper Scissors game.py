b0676ba387e847ce1b67e361e8627d41f2623f59
Linguist-Avik <124483988+Linguist-Avik@users.noreply.github.com>
2023-03-10 00:04:02 +0530

    Add files via upload

---
+++ Rock Paper Scissors game.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#rock paper scissors game
+import random
+select = int(input('Enter numbers 1 for rock or 2 for paper or 3 for scissors: '))
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+#logic for user input
+if select == 1:
+  print(rock)
+elif select == 2:
+  print(paper)
+else:
+  print(scissors)
+
+#logic for computer input
+random_selection = random.randint(1,3)
+print(f'Computer selects {random_selection}')
+if random_selection == 1:
+  print(rock)
+elif random_selection == 2:
+  print(paper)
+else:
+  print(scissors)
+  
+#logic for winner selection
+if select == random_selection:
+  print("It is a tie")
+elif select == 1 and random_selection == 2:
+  print("Computer wins")
+elif select == 1 and random_selection == 3:
+  print("You win")
+elif select == 2 and random_selection == 1:
+  print("You win")
+elif select == 2 and random_selection == 3:
+  print("Computer wins")
+elif select == 3 and random_selection == 1:
+  print("Computer wins")
+elif select == 3 and random_selection == 2:
+  print("You win")
+else:
+  print('wrong input')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
