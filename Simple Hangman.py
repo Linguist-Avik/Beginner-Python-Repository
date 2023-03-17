79b794e912713399a221158c652377ba0f2dee19
Linguist-Avik <124483988+Linguist-Avik@users.noreply.github.com>
2023-03-18 00:34:54 +0530

    Add files via upload

---
+++ Simple Hangman.py
@@ -0,0 +1,102 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#'lives' to keep track of the number of lives left. 
+lives = 6
+
+#Testing code / viewing the word
+#print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #check if alphabet guessed correctly
+    #check if nomore lives are left then game lost
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+          end_of_game = True
+          print("You lose.")
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+      end_of_game = True
+      print("You win.")
+
+    #printing out the ASCII stages
+    print(stages[lives])
